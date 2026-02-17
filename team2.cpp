82b091063663dde8e551696d3d727c87dec0cc70
jsode64 <jsode6405@gmail.com>
2026-02-17 14:05:23 -0500

    Added shark and reaction test

--- team2.cpp
+++ team2.cpp
@@ -1,7 +1,20 @@
 #include "team2.h"
 
+#include "reaction.h"
+
 
 //this should be at the bottom
 std::vector<captchaInfo> team2_get_captchas() {
-    return {};
+    return {
+        {
+            "Reaction Test",
+            800,
+            450,
+            &reaction_create_fn,
+            &reaction_draw_fn,
+            &reaction_mouse_click_fn,
+            &reaction_check_success_fn,
+            &reaction_get_instructions_fn,
+        }
+    };
 }
