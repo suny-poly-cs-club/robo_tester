98a2cef9b6e23263c7c06749e5979b8360599a4c
jSdCool <jSdCoolgaming@gmail.com>
2026-02-17 13:42:01 -0500

    team 2 labels for projs

--- team2.cpp
+++ team2.cpp
@@ -1,7 +1,33 @@
 #include "team2.h"
 
 
+//shark
+
+//stock market
+
+
+//harm the huma
+
+
+
+//teacton time
+
+
+
 //this should be at the bottom
 std::vector<captchaInfo> team2_get_captchas() {
-    return {};
+    return {
+        //shark
+
+        //stock market
+
+
+        //harm the huma
+
+
+
+        //teacton time
+
+
+    };
 }
