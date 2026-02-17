1ca0b7982291d903cac2bb9e3f9b254134ee51c3
jSdCool <jSdCoolgaming@gmail.com>
2026-02-17 14:04:09 -0500

    stock market funds

--- team2.cpp
+++ team2.cpp
@@ -4,7 +4,38 @@
 //shark
 
 //stock market
+struct stockMarketState {
+    int funds;
+};
 
+void * stockMarketCreate() {
+
+    return malloc(sizeof(stockMarketState));
+}
+
+void stockMarketDraw(void * state, int x, int y) {
+    auto * s = static_cast<stockMarketState*>(state);
+    DrawRectangle(x,y,500,500,BLACK);
+    std::string fundsText = "Funds: $";
+    fundsText += std::to_string(s->funds);
+    if (s->funds < 0) {
+        DrawText(fundsText.c_str(),x+10,y+20,30,RED);
+    } else {
+        DrawText(fundsText.c_str(),x+10,y+20,30,GREEN);
+    }
+}
+
+void stockMarketClicked(void * state, int button, int mouseX, int mouseY) {
+
+}
+
+bool stockMarkeySuccess(void * state) {
+    return true;
+}
+
+std::string stockMarketGetInstructions(void * state) {
+    return "Make $500 in the socket market";
+}
 
 //harm the huma
 
@@ -20,7 +51,15 @@ std::vector<captchaInfo> team2_get_captchas() {
         //shark
 
         //stock market
-
+        {
+            "stock_market",
+            500, 500,
+            &stockMarketCreate, // create function
+            &stockMarketDraw, // draw function
+            &stockMarketClicked, //mouse clicked function
+            &stockMarkeySuccess, // success function
+            &stockMarketGetInstructions,//instructions function
+        }
 
         //harm the huma
 
